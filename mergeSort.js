119d8574e49a243c90ecca3f656de875b607c720
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-03 15:14:58 -0600

    Add comments and refactor variables for readability

--- mergeSort.js
+++ mergeSort.js
@@ -1,26 +1,34 @@
 function mergeSort(arr) {
+  //  Array is sorted when it only has 1 element
   if (arr.length === 1) return arr;
 
+  //  Sort both left and right sides into a single array
   const aux = mergeSort(arr.slice(0, Math.round(arr.length / 2))).concat(
     mergeSort(arr.slice(Math.round(arr.length / 2))),
   );
 
+  //  Clear array
   arr.splice(0, arr.length);
-  let i = 0;
-  let j = Math.round(aux.length / 2);
-  while (i < Math.round(aux.length / 2) || j < arr.length) {
-    if (aux[i] < aux[j]) {
-      arr.push(aux[i]);
-      i += 1;
+
+  let leftPointer = 0;
+  const leftLimit = Math.round(aux.length / 2);
+  let rightPointer = leftLimit;
+  const rightLimit = aux.length;
+
+  while (leftPointer < leftLimit || rightPointer < rightLimit) {
+    if (aux[leftPointer] < aux[rightPointer]) {
+      arr.push(aux[leftPointer]);
+      leftPointer += 1;
     } else {
-      arr.push(aux[j]);
-      j += 1;
+      arr.push(aux[rightPointer]);
+      rightPointer += 1;
     }
-    if (i === Math.round(aux.length / 2)) {
-      return arr.concat(aux.slice(j));
+    //  When the limit of one of the sides is reached the rest of the other is merged
+    if (leftPointer === leftLimit) {
+      return arr.concat(aux.slice(rightPointer, rightLimit));
     }
-    if (j === aux.length) {
-      return arr.concat(aux.slice(i, Math.round(aux.length / 2)));
+    if (rightPointer === rightLimit) {
+      return arr.concat(aux.slice(leftPointer, leftLimit));
     }
   }
 
