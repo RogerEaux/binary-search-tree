ac69b2523d54895c822127bfe6581c3f4920f4b6
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-03 16:51:53 -0600

    Add function to log a tree representation

---
+++ index.js
@@ -0,0 +1,5 @@
+import createBST from './tree.js';
+
+const tree = createBST([3, 5, 7, 9, 1, 2, 3, 4, 1]);
+
+tree.prettyPrint();
--- tree.js
+++ tree.js
@@ -32,8 +32,22 @@ function createBST(array) {
 
   let root = buildTree(prepArray(array));
 
+  function prettyPrint(node = root, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
   return {
     root,
+    prettyPrint,
   };
 }
 
